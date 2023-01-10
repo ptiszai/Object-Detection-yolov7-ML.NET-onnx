d84ddcc2f437201acc0233288edd816c5631e422
Tiszai Istvan <tiszaii@hotmail.com>
2023-01-10 19:43:12 +0100

    V 1.0
    Version 1.0

--- Program.cs
+++ Program.cs
@@ -1,28 +1,26 @@
 ﻿using System.Drawing;
-using System.IO;
 using System.Diagnostics;
 
 using Emgu.CV;
 using Emgu.CV.Structure;
-using Emgu.CV.CvEnum;
+
 using Yolov5Net.Scorer.Models;
 using Yolov5Net.Scorer;
-using System;
-using System.Reflection.Emit;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Formats.Asn1.AsnWriter;
 
-
+//https://emgu.com/wiki/index.php/Camera_Capture
 namespace Object_Detection_yolov7_ML.NET
-{    
+{
     internal class Program
-    {       
-        static readonly string assetsPath = GetAbsolutePath(@"../../../");
-        static readonly string modelFilePath = Path.Combine(assetsPath, "models", "yolov7-tiny-norm.onnx");
-        static readonly string imagesFolder = Path.Combine(assetsPath, "images");
-        static readonly string outputFolder = Path.Combine(assetsPath,  "output");
-        static String windowName = "Your Captcha";
-        static Stopwatch stopwatch = new Stopwatch();
+    {
+        private string assetsPath = "";
+        //private bool queueFrameEnd = false;
+        /*assetsPath = GetAbsolutePath(@"../../../");
+        private string modelFilePath = Path.Combine(assetsPath, "models", "yolov7-tiny-norm.onnx");
+        private string imagesFolder = Path.Combine(assetsPath, "images");
+        private string outputFolder = Path.Combine(assetsPath,  "output");*/
+        //private string windowName = "Your Captcha";
+        //private Stopwatch stopwatch = new Stopwatch();
+        //private VideoCapture videoCapture;
 
         //-------------------------------------------------------
         private static string GetAbsolutePath(string relativePath)
@@ -61,62 +59,181 @@ namespace Object_Detection_yolov7_ML.NET
             return cvImage.Mat;
         }
 
-//******************************************************************************
-//* MAIN
-//******************************************************************************
+        //******************************************************************************
+        //* MAIN
+        //******************************************************************************
         static void Main(string[] args)
         {
             bool wr = true;
+            bool mp4 = true;
+            bool img = false;
+            string assetsPath = GetAbsolutePath(@"../../../");
+            string modelFilePath = Path.Combine(assetsPath, "models", "yolov7-tiny-norm.onnx");
+            string imagesFolder = Path.Combine(assetsPath, "images");
+            string outputFolder = Path.Combine(assetsPath, "output");           
+            VideoWriter? videowriter = null ;
+            string windowName = "Your Captcha";
+            Stopwatch stopwatch = new Stopwatch();
+            Queue<Mat>? query = null;
+            QueueFrame? queryFrame = null;
+            bool queueFrameEnd = false;
 
-            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Yolov7 + ML.NET!");                               
 
             if (!Directory.Exists(outputFolder))
             {
                 Directory.CreateDirectory(outputFolder);
             }
+
+
             var imgs = Directory.GetFiles(imagesFolder).Where(filePath => Path.GetExtension(filePath) == ".jpg");
             //stopwatch.Start();
-            using var scorer = new YoloScorer<YoloCocoP5Model>(modelFilePath); //460 ms
+            YoloScorer<YoloCocoP5Model> scorer = new YoloScorer<YoloCocoP5Model>(modelFilePath); //460 ms
             //stopwatch.Stop();
             //long elapsed_time = stopwatch.ElapsedMilliseconds;
             //Console.WriteLine($"Inference time {elapsed_time}!");
 
-
-            foreach (var imgsFile in imgs)
+            if (mp4)
             {
-                stopwatch.Start();
-                using var image = System.Drawing.Image.FromFile(imgsFile);
-                //stopwatch.Start();
-                List<YoloPrediction> predictions = scorer.Predict(image);
-                using var graphics = Graphics.FromImage(image);
-                stopwatch.Stop();
-                long elapsed_time = stopwatch.ElapsedMilliseconds;
-                string label = $"Inference time {elapsed_time} ms!";
-                //Console.WriteLine(label);                
-                foreach (var prediction in predictions)
+                try
                 {
-                    double score = Math.Round(prediction.Score, 2);
-                    graphics.DrawRectangles(new Pen(prediction.Label.Color, 3), new[] { prediction.Rectangle });
-                    var (x, y) = (prediction.Rectangle.X - 3, prediction.Rectangle.Y - 23);
-                    graphics.DrawString($"{prediction.Label.Name} ({score})",new Font("Consolas", 16, GraphicsUnit.Pixel), new SolidBrush(prediction.Label.Color),new PointF(x, y));
+                    query = new Queue<Mat>();
+                    queryFrame = new QueueFrame(ref query);
+                    queryFrame.Ended += (mode) =>
+                    {
+                        queueFrameEnd = true;
+                    };
+               
+                    string video_fileName = imagesFolder + "/cars.mp4";
+                    if (!File.Exists(video_fileName))
+                    {
+                        Console.WriteLine($"ERROR:video file not founded:{video_fileName}!");
+                        return;
+                    }
+                    if (!queryFrame.StartReadFrames(video_fileName))
+                    {
+                        Console.WriteLine($"ERROR:not started:{video_fileName}!");
+                        return;
+                    }
+                    Thread.Sleep(1000);
+                    if (queryFrame.IfStartedReadFrames())
+                    {
+                        if (wr)
+                        {
+                            FileInfo fi = new FileInfo("cars.mp4");
+                            string name = fi.Name;
+                            string videopath_out = Path.Combine(outputFolder, $"result_{name}");
+                            int codec = VideoWriter.Fourcc('m', 'p', '4', 'v');
+                            int fps = 10;
+                            Size size = queryFrame.GetSize();
+                            videowriter = new VideoWriter(videopath_out, codec, fps, size, true);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR:not started thread for video writing!");
+                        return;
+                    }
+                    while (true)
+                    {
+                        if (query.Count>0)
+                        {
+                            Console.WriteLine($"query.Count:{query.Count}");
+                            Mat frame = query.Dequeue();
+                            stopwatch.Restart();
+                            Bitmap image = frame.ToImage<Bgr, Byte>().ToBitmap();
+                            List<YoloPrediction> predictions = scorer.Predict(image);                            
+                            using var graphics = Graphics.FromImage(image);
+                            stopwatch.Stop();
+                            long elapsed_time = stopwatch.ElapsedMilliseconds;                            
+                            string label = $"Inference time {elapsed_time} ms!"; // 450-500 ms
+                            Console.WriteLine(label);
+                            queryFrame.SetSleepMs(elapsed_time);
+                            //stopwatch.Restart();
+                            foreach (var prediction in predictions)
+                            {
+                                double score = Math.Round(prediction.Score, 2);
+                                graphics.DrawRectangles(new Pen(prediction.Label.Color, 3), new[] { prediction.Rectangle });
+                                var (x, y) = (prediction.Rectangle.X - 3, prediction.Rectangle.Y - 23);
+                                graphics.DrawString($"{prediction.Label.Name} ({score})", new Font("Consolas", 16, GraphicsUnit.Pixel), new SolidBrush(prediction.Label.Color), new PointF(x, y));
+                            }
+                            graphics.DrawString(label, new Font("Consolas", 16, GraphicsUnit.Pixel), new SolidBrush(Color.White), new PointF(0, 15));
+                            Mat cvImage = GetMatFromSDImage(image);
+                            //stopwatch.Stop();
+                           // Console.WriteLine($"predictiont ms:{stopwatch.ElapsedMilliseconds}"); // 10-15 ms
+                            if (wr)
+                            {
+                                videowriter.Write(frame); !!!!!!!!!!!!!!
+                            }
+                            //stopwatch.Stop();
+                           // Console.WriteLine($"predictiont+wr ms:{stopwatch.ElapsedMilliseconds}"); // 19-25 ms
+                            CvInvoke.Imshow(windowName, cvImage);
+                        }
+                        if (queueFrameEnd && query.Count < 1)
+                        {
+                            Console.WriteLine("frames process END!");
+                            break;
+                        }
+                        if (CvInvoke.PollKey()>0)
+                        {
+                            queryFrame.StopReadFrames();
+                            break;
+                        }
+                        Thread.Sleep(10);                       
+                    }
+                    if (videowriter != null)
+                    {
+                        videowriter.Dispose();
+                    }
                 }
-                graphics.DrawString(label, new Font("Consolas", 16, GraphicsUnit.Pixel), new SolidBrush(Color.White), new PointF(0, 15));                
-                if (wr)
+                catch (Exception ex)
                 {
-                    FileInfo fi = new FileInfo(imgsFile);
-                    string name = fi.Name;                 
-                    image.Save(Path.Combine(outputFolder, $"redsult_{name}"));
+                    //Program.mutex.ReleaseMutex();
+                    Console.WriteLine($"ERROR:video process {ex.Message}");
+                    if (queryFrame != null)
+                    {
+                        queryFrame.StopReadFrames();
+                    }
                 }
-
-                Mat cvImage = GetMatFromSDImage(image);                         
-
-                //stopwatch.Stop();
-                //long elapsed_time = stopwatch.ElapsedMilliseconds;
-                // Console.WriteLine($"Inference time {elapsed_time}!");
-                CvInvoke.Imshow(windowName, cvImage);
-                CvInvoke.WaitKey(0);
             }
+            else if (img)
+            {          
+                foreach (var imgsFile in imgs)
+                {
+                    stopwatch.Start();
+                    using var image = System.Drawing.Image.FromFile(imgsFile);
+                    //stopwatch.Start();
+                    List<YoloPrediction> predictions = scorer.Predict(image);
+                    using var graphics = Graphics.FromImage(image);
+                    stopwatch.Stop();
+                    long elapsed_time = stopwatch.ElapsedMilliseconds;
+                    string label = $"Inference time {elapsed_time} ms!";
+                    //Console.WriteLine(label);                
+                    foreach (var prediction in predictions)
+                    {
+                        double score = Math.Round(prediction.Score, 2);
+                        graphics.DrawRectangles(new Pen(prediction.Label.Color, 3), new[] { prediction.Rectangle });
+                        var (x, y) = (prediction.Rectangle.X - 3, prediction.Rectangle.Y - 23);
+                        graphics.DrawString($"{prediction.Label.Name} ({score})", new Font("Consolas", 16, GraphicsUnit.Pixel), new SolidBrush(prediction.Label.Color), new PointF(x, y));
+                    }
+                    graphics.DrawString(label, new Font("Consolas", 16, GraphicsUnit.Pixel), new SolidBrush(Color.White), new PointF(0, 15));
+                    if (wr)
+                    {
+                        FileInfo fi = new FileInfo(imgsFile);
+                        string name = fi.Name;
+                        image.Save(Path.Combine(outputFolder, $"redsult_{name}"));
+                    }
 
+                    Mat cvImage = GetMatFromSDImage(image);
+
+                    //stopwatch.Stop();
+                    //long elapsed_time = stopwatch.ElapsedMilliseconds;
+                    // Console.WriteLine($"Inference time {elapsed_time}!");
+                    CvInvoke.Imshow(windowName, cvImage);
+                    //CvInvoke.WaitKey(0);
+                }
+            }
+            CvInvoke.WaitKey(0);
         }
     }
 }
---
+++ QueueFrame.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Threading;
+using System.Drawing;
+using Emgu.CV;
+using System.Diagnostics.Metrics;
+
+namespace Object_Detection_yolov7_ML.NET
+{
+    public class QueueFrame : IDisposable
+    {
+        #region public eventHandlers
+        public Action<bool> Ended;       
+        #endregion
+
+        #region private variables 
+        private Queue<Mat> queryFrame;
+        private Thread? thread;
+        private VideoCapture? videoCapture;        
+        private long sleep_ms = 200;
+        private bool run = false;
+        private int frameWidth;
+        private int frameHeight;
+        private int totFrames;
+        private int fcount = 1;
+        #endregion
+
+        #region public functions
+        //-----------------------
+        public QueueFrame(ref Queue<Mat> queryFrame_a)
+        {
+            queryFrame = queryFrame_a;
+            thread = new Thread(executor);
+        }
+
+        //-----------------------
+        public bool StartReadFrames(string filename_a)
+        {
+            videoCapture = new VideoCapture(filename_a);
+            if ((videoCapture == null) || (!videoCapture.Grab()))
+            {
+                Console.WriteLine("ERROR: start frame grab!");
+                return false;
+            }
+            queryFrame.Clear();
+            run = true;
+            thread.Start();
+            return true;
+        }
+
+        //-----------------------
+        public bool IfStartedReadFrames()
+        {
+            if (thread.IsAlive && run && fcount > 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //-----------------------
+        public void StopReadFrames()
+        {
+            if (thread.IsAlive)
+            {
+                run = false;
+            //    thread.Abort();
+                queryFrame.Clear();
+            }
+        }
+
+        public void SetSleepMs(long sleep_ms_a)
+        {
+            sleep_ms = sleep_ms_a;
+        }
+
+        //-----------------------
+        public void Dispose()
+        {
+            if (videoCapture != null)
+            {
+                videoCapture.Dispose();
+            }
+            //videowriter.Dispose();
+            if (thread.IsAlive)
+            {
+                run = false;
+                thread.Abort();
+            }
+        }
+
+        //-----------------------
+        public Size GetSize()
+        {
+            Size result = new Size(frameWidth, frameHeight);
+            return result;
+        }
+        #endregion
+        #region private functions       
+        //-----------------------
+        private void executor()
+        {
+            totFrames = (int)videoCapture.Get(Emgu.CV.CvEnum.CapProp.FrameCount);
+            frameWidth = (int)videoCapture.Get(Emgu.CV.CvEnum.CapProp.FrameWidth);
+            frameHeight = (int)videoCapture.Get(Emgu.CV.CvEnum.CapProp.FrameHeight);
+            //videoCapture.Set(Emgu.CV.CvEnum.CapProp.FrameWidth, 1280);
+            //videoCapture.Set(Emgu.CV.CvEnum.CapProp.FrameHeight, 720);            
+
+            try
+            {
+                while (run)
+                {
+                    if (videoCapture.IsOpened)
+                    {                    
+                        Mat frame = videoCapture.QueryFrame();
+                        //videoCapture.Read(frame);
+                        if (frame == null)
+                        {
+                            run = false;
+                            if (totFrames >= fcount)
+                            {
+                                Console.WriteLine($"Video read frames END, queryFrame.Count:{queryFrame.Count}");                             
+                                Ended?.Invoke(true);
+                            }
+                            else
+                            {
+                                Console.WriteLine("ERROR:frame grab false!");                               
+                                queryFrame.Clear();
+                            }
+                            break;
+                        }
+                        queryFrame.Enqueue(frame);
+                        fcount++;
+                        Console.WriteLine($"frame number {fcount}");                   
+                    }
+                    Thread.Sleep((int)sleep_ms);                  
+                }
+            }
+            catch (ThreadAbortException ex)
+            {
+                //Program.mutex.ReleaseMutex();
+                Console.WriteLine($"ERROR:QueueFrame:thread.executor {ex.Message}");
+                run = false;
+            }
+        }
+        #endregion
+    }
+}
